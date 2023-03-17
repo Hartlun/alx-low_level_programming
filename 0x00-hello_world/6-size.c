165c7d18d889c7d645bc17b68a5312b94e2b3a92
hartlun <hartlun@gmail.com>
2023-03-17 02:52:15 -0700

    my second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 /**
- * main = Starting point
- * Return: Always 0
+ * main = A program that prints various sizes
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("size of a char: %zu byte(s)\n", size of (char));
-	printf("size of a int: %zu byte(s)\n", size of int));
-	printf("size of a long int: %zu byte(s)\n", size of (long int));
-	printf("size of a long long int: %zu byte(s)\n", size of (long long int));
-	printf("size of a float: %zu byte(s)\n", size of (float));
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unasigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unasigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unasigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unasigned long)sizeof(f));
+return (0);
 }
