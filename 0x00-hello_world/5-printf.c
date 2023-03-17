902187bec4db654519c3eed8a2e3e9e25aa10b92
hartlun <hartlun@gmail.com>
2023-03-17 16:08:35 -0700

    my last commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/**
- * main = My program runs smoothly without the puts function
- * Return: 0
- */
 
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
