f9b1ed67da9607bd7b189242f7d8ed500042ece0
hartlun <hartlun@gmail.com>
2023-03-16 18:06:29 -0700

    my secnd commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main = Starting point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", size of (char));
+	printf("size of a char: %zu byte(s)\n", size of (char));
+	printf("size of a char: %zu byte(s)\n", size of (long intt));
+	printf("size of a long long int: %zu byte(s)\n", size of (long long int));
+	printf("size of a float: %zu byte(s)\n", size of (float));
+	return (0);
+}
