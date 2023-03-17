960ee832f74883db95bb05c5a4acdbf5cb2ca59f
hartlun <hartlun@gmail.com>
2023-03-16 18:26:12 -0700

    advance task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	printf("size of a char: %zu byte(s)\n", size of (char));
-	printf("size of a char: %zu byte(s)\n", size of (char));
-	printf("size of a char: %zu byte(s)\n", size of (long intt));
+	printf("size of a int: %zu byte(s)\n", size of int));
+	printf("size of a long int: %zu byte(s)\n", size of (long int));
 	printf("size of a long long int: %zu byte(s)\n", size of (long long int));
 	printf("size of a float: %zu byte(s)\n", size of (float));
 	return (0);
