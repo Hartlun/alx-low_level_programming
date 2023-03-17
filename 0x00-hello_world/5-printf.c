c569247fcdf8c4c82b2bed1fb994879a3b592a9d
hartlun <hartlun@gmail.com>
2023-03-16 17:31:38 -0700

    My first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main = My program runs smoothly without the puts function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
