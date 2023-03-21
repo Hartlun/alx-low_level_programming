c508a0fbfaa9a1f65eb984c135b05a9639067d58
hartlun <hartlun@gmail.com>
2023-03-21 15:27:58 -0700

    my nested loop commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
 void times_table(void)
 {
 	int a, b, c;
+	
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -35,7 +36,7 @@ void times_table(void)
 				if (b < 9)
 				{
 					_putchar(',');
-					_putchar(',');
+					_putchar(' ');
 				}
 			}
 		}
