62332f6f8452abd3964275c064c87bdafe6b867f
hartlun <hartlun@gmail.com>
2023-03-21 17:13:13 -0700

    my nested loop commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+/**
+ * nt _islower - check ifthe character is lower case
+ * description: the character
+ * Return: 1 if the letter is lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
