861113015192a3883554304c5a662212c7e2e9e7
hartlun <hartlun@gmail.com>
2023-03-21 17:43:54 -0700

    my nested loop commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _abs - absolute value
+ * Description: Absoute value of an integer
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
